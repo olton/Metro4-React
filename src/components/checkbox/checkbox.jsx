fcbe43f12959bea09ecf2aedb4298450601a818c
Sergey Pimenov <sergey@pimenov.com.ua>
2019-11-22 14:45:34 +0200

    upd checkbox, radio for add getDerivedStateFromProps

--- src/components/checkbox/checkbox.jsx
+++ src/components/checkbox/checkbox.jsx
@@ -21,6 +21,31 @@ export default class Checkbox extends React.Component {
         onUnCheck: ()=>{}
     };
 
+    constructor(props){
+        super(props);
+        this.state = {
+            initChecked: props.checked,
+            checked: props.checked
+        }
+    }
+
+    static getDerivedStateFromProps(props, state){
+        if (props.checked !== state.initChecked) {
+            return {
+                checked: props.checked,
+                initChecked: props.checked
+            }
+        }
+        return null;
+    }
+
+    onClick = e => {
+        this.setState({
+            checked: !this.state.checked
+        });
+        e.preventDefault();
+    };
+
     onChangeHandler = (e) => {
         const state = e.target.checked;
         this.props.onChange(e);
@@ -31,7 +56,7 @@ export default class Checkbox extends React.Component {
         const {
             transition,
             mode,
-            checked,
+            checked: propsChecked,
             caption,
             variant,
             cls, className,
@@ -42,12 +67,13 @@ export default class Checkbox extends React.Component {
             onUnCheck,
             ...input
         } = this.props;
+        const {checked} = this.state;
 
         const checkboxMode = mode === "switch" ? `switch${(variant === 2 ? '-material' : '')}` : `checkbox ${(variant === 2 ? 'style2' : '')}`;
 
         return (
-            <label className={`${checkboxMode} ${transition ? "transition-on" : ""} ${cls} ${className}`}>
-                <input type="checkbox" { ...input } defaultChecked={checked} onChange={this.onChangeHandler}/>
+            <label className={`${checkboxMode} ${transition ? "transition-on" : ""} ${cls} ${className}`} onClick={this.onClick}>
+                <input type="checkbox" { ...input } checked={checked} onChange={this.onChangeHandler}/>
                 <span className={ 'check ' + clsCheck } />
                 <span className={ 'caption ' + clsCaption }>{caption}</span>
             </label>
--- src/components/radio/radio.jsx
+++ src/components/radio/radio.jsx
@@ -17,6 +17,32 @@ export default class Radio extends React.Component {
         onUnCheck: ()=>{}
     };
 
+    constructor(props){
+        super(props);
+        this.state = {
+            initChecked: props.checked,
+            checked: props.checked
+        }
+    }
+
+    static getDerivedStateFromProps(props, state){
+        if (props.checked !== state.initChecked) {
+            return {
+                checked: props.checked,
+                initChecked: props.checked
+            }
+        }
+        return null;
+    }
+
+    onClick = e => {
+        if (this.state.checked) return;
+        this.setState({
+            checked: !this.state.checked
+        });
+        e.preventDefault();
+    };
+
     onChangeHandler = (e) => {
         const state = e.target.checked;
         this.props.onChange(e);
@@ -25,7 +51,7 @@ export default class Radio extends React.Component {
 
     render(){
         const {
-            checked,
+            checked: propsChecked,
             caption,
             variant,
             clsCheckbox,
@@ -36,10 +62,11 @@ export default class Radio extends React.Component {
             onChange,
             ...input
         } = this.props;
+        const {checked} = this.state;
 
         return (
-            <label className={'radio' + clsCheckbox + ' ' + (variant === 2 ? 'style2' : '') + ' transition-on'}>
-                <input type="radio" {...input} defaultChecked={checked} onChange={this.onChangeHandler}/>
+            <label className={'radio' + clsCheckbox + ' ' + (variant === 2 ? 'style2' : '') + ' transition-on'} onClick={this.onClick}>
+                <input type="radio" {...input} checked={checked} onChange={this.onChangeHandler}/>
                 <span className={"check " + clsCheck}/>
                 <span className={"caption " + clsCaption}>{caption}</span>
             </label>
